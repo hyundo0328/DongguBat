a2f842778be2a2edea9aff389217885f8fb78e83
hyundo0328 <hyundo13@naver.com>
2023-10-29 22:37:38 +0900

    배너창 자동 출력

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -6,10 +6,6 @@ import 'dart:async';
 import 'login.dart';
 import 'mypage.dart';
 
-void main() {
-  runApp(MainPage());
-}
-
 class MainPage extends StatefulWidget {
   @override
   State<MainPage> createState() => _MainPageState();
@@ -20,23 +16,40 @@ class _MainPageState extends State<MainPage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   PageController _pageController = PageController(initialPage: 0);
+
+  int _activePage = 0;
+
+  final List<Widget> _pages = [
+    Expanded(
+        child: Container(
+      child: Image.asset("assets/rainbow.jpg"),
+    )),
+    Expanded(
+        child: Container(
+      child: Image.asset("assets/vertical_symnbol.jpg"),
+    )),
+    Expanded(
+        child: Container(
+      child: Image.asset("assets/rainbow.jpg"),
+    )),
+  ];
+
   int _currentPage = 0;
-  int _numberOfPages = 3; // 페이지 수
-  int _milliseconds = 2000; // 넘어가는 간격 (2초)
+  int tmp = 0;
+  int _numberOfPages = 1000000; // 페이지 수
+  int _milliseconds = 10000; // 넘어가는 간격 (2초)
 
   @override
   void initState() {
     super.initState();
     // 타이머를 사용하여 페이지 자동으로 넘기기
-    Timer.periodic(Duration(milliseconds: _milliseconds), (Timer timer) {
-      if (_currentPage < _numberOfPages - 1) {
-        _currentPage++;
-      } else {
-        _currentPage = 0;
-      }
+    Timer.periodic(Duration(milliseconds: 2000), (Timer timer) {
+      _currentPage++;
+      _currentPage = _currentPage % 3;
       // print(_currentPage);
+      
       _pageController.animateToPage(_currentPage,
-          duration: Duration(milliseconds: 500), curve: Curves.easeOut);
+          duration: Duration(milliseconds: 500), curve: Curves.ease);
     });
   }
 
@@ -78,7 +91,7 @@ class _MainPageState extends State<MainPage> {
             ],
           )),
           body: Padding(
-              padding: EdgeInsets.all(15.0),
+              padding: EdgeInsets.all(20.0),
               child: Builder(builder: (context) {
                 return GestureDetector(
                   onTap: () {
@@ -89,63 +102,58 @@ class _MainPageState extends State<MainPage> {
                       Expanded(
                         flex: 1,
                         child: Stack(
+                          fit: StackFit.loose,
                           children: [
-                            PageView(
-                              controller: _pageController,
-                              children: <Widget>[
-                                // 각 페이지에 원하는 내용을 추가
-                                Container(
-                                  child: Text("1페이지"),
-                                ),
-                                Container(
-                                  child: Text("2페이지"),
-                                ),
-                                Container(
-                                  child: Text("3페이지"),
-                                ),
-                              ],
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              child: PageView.builder(
+                                controller: _pageController,
+                                
+                                onPageChanged: (int page) {
+                                  setState(() {
+                                    _activePage = page;
+                                  });
+                                },
+                                //itemCount: _pages.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return _pages[index % _pages.length];
+                                },
+                              ),
                             ),
-                            Align(
-                              alignment: Alignment.bottomCenter,
-                              child: Container(
-                                margin: EdgeInsets.symmetric(vertical: 2.0),
+                            Positioned(
+                              height: 200,
+                              bottom: 5,
+                              left: 0,
+                              right: 0,
+                              child: Align(
+                                alignment: Alignment.bottomCenter,
                                 child: Row(
                                   mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      margin: EdgeInsets.all(4.0),
-                                      width: 7.0,
-                                      height: 7.0,
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: _currentPage == 0
-                                            ? Colors.grey
-                                            : Colors.grey.withOpacity(0.5),
-                                      ),
-                                    ),
-                                    Container(
-                                      margin: EdgeInsets.all(4.0),
-                                      width: 7.0,
-                                      height: 7.0,
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: _currentPage == 1
-                                            ? Colors.grey
-                                            : Colors.grey.withOpacity(0.5),
-                                      ),
-                                    ),
-                                    Container(
-                                      margin: EdgeInsets.all(4.0),
-                                      width: 7.0,
-                                      height: 7.0,
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: _currentPage == 2
-                                            ? Colors.grey
-                                            : Colors.grey.withOpacity(0.5),
-                                      ),
-                                    ),
-                                  ],
+                                  verticalDirection: VerticalDirection.down,
+                                  children: List<Widget>.generate(
+                                      _pages.length,
+                                      (index) => Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 3),
+                                            child: InkWell(
+                                              onTap: () {
+                                                _pageController.animateToPage(
+                                                    index,
+                                                    duration: Duration(
+                                                        milliseconds: 300),
+                                                    curve: Curves.easeIn);
+                                              },
+                                              child: CircleAvatar(
+                                                radius: 3,
+                                                backgroundColor:
+                                                    _activePage == index
+                                                        ? Colors.grey
+                                                        : Colors.grey
+                                                            .withOpacity(0.5),
+                                              ),
+                                            ),
+                                          )),
                                 ),
                               ),
                             ),
@@ -159,7 +167,6 @@ class _MainPageState extends State<MainPage> {
                               Expanded(
                                 child: Container(
                                   margin: EdgeInsets.symmetric(vertical: 8.0),
-                                  padding: EdgeInsets.all(8.0),
                                   alignment: Alignment.center,
                                   decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(
