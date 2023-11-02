9ec8ea062d37340121eb785b5d02c2da8d043e34
hyundo0328 <hyundo13@naver.com>
2023-11-02 19:51:22 +0900

    위젯 분리, 목록으로 Drawer 실패

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,13 +1,10 @@
 //main.dart
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import '../widgets/widget_appbar.dart';
+import '../widgets/widget_bottombar.dart';
+import '../widgets/widget_drawer.dart';
 import 'dart:async';
-import 'login.dart';
-import 'my_page.dart';
-import 'communicate.dart';
-import 'my_page.dart';
-import 'program_rent.dart';
+
 
 class MainPage extends StatefulWidget {
   @override
@@ -21,26 +18,7 @@ class _MainPageState extends State<MainPage> {
   PageController _pageController = PageController(initialPage: 0);
 
   int _activePage = 0;
-
-  final List<Widget> _pages = [
-    Expanded(
-        child: Container(
-      child: Image.asset("assets/세이프 라이딩.jpg"),
-    )),
-    Expanded(
-        child: Container(
-      child: Image.asset("assets/플로깅플레져.jpg"),
-    )),
-    Expanded(
-        child: Container(
-      child: Image.asset("assets/SNS 인증샷.jpg"),
-    )),
-  ];
-
   int _currentPage = 0;
-  int tmp = 0;
-  int _numberOfPages = 1000000; // 페이지 수
-  int _milliseconds = 10000; // 넘어가는 간격 (2초)
 
   @override
   void initState() {
@@ -49,7 +27,6 @@ class _MainPageState extends State<MainPage> {
     Timer.periodic(Duration(milliseconds: 2000), (Timer timer) {
       _currentPage++;
       _currentPage = _currentPage % 3;
-      // print(_currentPage);
 
       _pageController.animateToPage(_currentPage,
           duration: Duration(milliseconds: 500), curve: Curves.ease);
@@ -57,227 +34,118 @@ class _MainPageState extends State<MainPage> {
   }
 
   @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          key: scaffoldKey,
-          // 상단바, 하단바, 메인화면으로 나눌때 주로 사용
-          appBar: AppBar(
-            title: Text('동구밭'),
-            centerTitle: true,
-            backgroundColor: const Color.fromARGB(255, 44, 96, 68),
-            toolbarHeight: 60,
-            // leading : 아이콘 버튼이나 간단한 위젯을 왼쪽에 배치할 때 사용
-
-            // actions : 복수의 아이콘 버튼 등을 오른쪽에 배치할 때 사용
-          ),
-          drawer: Drawer(
-              child: ListView(
-            padding: EdgeInsets.all(10),
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: AssetImage('assets/vertical_symnbol.jpg'),
-                ),
-                accountName: Text('hyundo'),
-                accountEmail: Text('hyundo13@naver.com'),
-                onDetailsPressed: () {
-                  print('arrow is clicked');
-                },
-              ),
-              ListTile(
-                title: Text('소통'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => Communicate()));
-                },
-              ),
-              ListTile(
-                title: Text('프로그램/대관'),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => program_apply()));
-                },
-              ),
-            ],
-          )),
-          body: Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Builder(builder: (context) {
-                return GestureDetector(
-                  onTap: () {
-                    FocusScope.of(context).unfocus();
-                  },
-                  child: Column(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Stack(
-                          fit: StackFit.loose,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0)),
-                              child: PageView.builder(
-                                controller: _pageController,
-
-                                onPageChanged: (int page) {
-                                  setState(() {
-                                    _activePage = page;
-                                  });
-                                },
-                                //itemCount: _pages.length,
-                                itemBuilder: (BuildContext context, int index) {
-                                  return _pages[index % _pages.length];
-                                },
-                              ),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        key: scaffoldKey,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(55.0), // AppBar의 원하는 높이로 설정
+          child: WidgetAppBar(title: "동구밭"),
+        ),
+        drawer: WidgetDrawer(),
+        body: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(15, 15, 15, 15),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 240,
+                child: Stack(
+                  fit: StackFit.loose,
+                  children: [
+                    PageView(
+                      // controller: _pageController,
+                      controller: _pageController ??=
+                          PageController(initialPage: 0),
+                      scrollDirection: Axis.horizontal,
+                      onPageChanged: (int page) {
+                        setState(() {
+                          _activePage = page;
+                        });
+                      },
+                      children: [
+                        Align(
+                          alignment: AlignmentDirectional(0.00, -1.00),
+                          child: ClipRRect(
+                            // borderRadius: BorderRadius.circular(8),
+                            child: Image.asset(
+                              'assets/SNS 인증샷.jpg',
+                              width: double.infinity,
+                              height: 250,
+                              fit: BoxFit.contain,
+                              alignment: Alignment(0.00, -1.00),
                             ),
-                            Positioned(
-                              height: 200,
-                              bottom: 5,
-                              left: 0,
-                              right: 0,
-                              child: Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  verticalDirection: VerticalDirection.down,
-                                  children: List<Widget>.generate(
-                                      _pages.length,
-                                      (index) => Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 3),
-                                            child: InkWell(
-                                              onTap: () {
-                                                _pageController.animateToPage(
-                                                    index,
-                                                    duration: Duration(
-                                                        milliseconds: 300),
-                                                    curve: Curves.easeIn);
-                                              },
-                                              child: CircleAvatar(
-                                                radius: 3,
-                                                backgroundColor:
-                                                    _activePage == index
-                                                        ? Colors.grey
-                                                        : Colors.grey
-                                                            .withOpacity(0.5),
-                                              ),
-                                            ),
-                                          )),
-                                ),
-                              ),
+                          ),
+                        ),
+                        Align(
+                          alignment: AlignmentDirectional(0.00, -1.00),
+                          child: ClipRRect(
+                            // borderRadius: BorderRadius.circular(8),
+                            child: Image.asset(
+                              'assets/세이프 라이딩.jpg',
+                              width: double.infinity,
+                              height: 250,
+                              fit: BoxFit.contain,
+                              alignment: Alignment(0.00, -1.00),
                             ),
-                          ],
+                          ),
+                        ),
+                        Align(
+                          alignment: AlignmentDirectional(0.00, -1.00),
+                          child: ClipRRect(
+                            // borderRadius: BorderRadius.circular(8),
+                            child: Image.asset(
+                              'assets/플로깅플레져.jpg',
+                              width: double.infinity,
+                              height: 225,
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Center(
+                      // heightFactor: 190,
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          verticalDirection: VerticalDirection.down,
+                          children: List<Widget>.generate(
+                              3,
+                              (index) => Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 3),
+                                    child: InkWell(
+                                      onTap: () {
+                                        _pageController.animateToPage(index,
+                                            duration:
+                                                Duration(milliseconds: 300),
+                                            curve: Curves.easeIn);
+                                      },
+                                      child: CircleAvatar(
+                                        radius: 3,
+                                        backgroundColor: _activePage == index
+                                            ? Colors.grey
+                                            : Colors.grey.withOpacity(0.5),
+                                      ),
+                                    ),
+                                  )),
                         ),
                       ),
-                      Expanded(
-                          flex: 4,
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  margin: EdgeInsets.symmetric(vertical: 8.0),
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(
-                                        10.0), // 원하는 둥근 정도를 설정합니다.
-                                    color: Colors.white,
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.grey.withOpacity(0.5),
-                                        spreadRadius: 2,
-                                        blurRadius: 4,
-                                        offset: Offset(0, 2),
-                                      ),
-                                    ],
-                                  ),
-                                  child: Text('2'),
-                                ),
-                              ),
-                              Expanded(
-                                child: Container(
-                                  margin: EdgeInsets.symmetric(vertical: 8.0),
-                                  padding: EdgeInsets.all(8.0),
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(
-                                        10.0), // 원하는 둥근 정도를 설정합니다.
-                                    color: Colors.white,
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.grey.withOpacity(0.5),
-                                        spreadRadius: 2,
-                                        blurRadius: 4,
-                                        offset: Offset(0, 2),
-                                      ),
-                                    ],
-                                  ),
-                                  child: Text('2'),
-                                ),
-                              ),
-                            ],
-                          )),
-                    ],
-                  ),
-                );
-              })),
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            iconSize: 35,
-            selectedItemColor: const Color.fromARGB(255, 44, 96, 68),
-            unselectedItemColor: Colors.grey,
-            selectedLabelStyle: TextStyle(fontSize: 10),
-            items: [
-              BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: '목록'),
-              BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
-              BottomNavigationBarItem(icon: Icon(Icons.person), label: '마이페이지'),
+                    ),
+                  ],
+                ),
+              ),
             ],
-            currentIndex: 1,
-            onTap: (int index) {
-              if (index == 0) {
-                if (scaffoldKey.currentState!.isDrawerOpen) {
-                  scaffoldKey.currentState!.closeDrawer();
-                } else {
-                  scaffoldKey.currentState!.openDrawer();
-                }
-              } else if (index == 1) {
-                // '홈' 아이콘을 클릭한 경우 홈 화면으로 이동
-                // Navigator.push(
-                //   context
-                //   MaterialPageRoute(
-                //     builder: (BuildContext context) => HomeScreen(),
-                //   ),
-                // );
-              } else if (index == 2) {
-                // '마이페이지' 아이콘을 클릭한 경우 마이페이지 화면으로 이동
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyPageScreen()),
-                );
-              }
-            },
-          )),
-    );
-  }
-
-  Widget buildBanner(String text, int index) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0), // 원하는 둥근 정도를 설정합니다.
-        color: Colors.blueGrey,
+          ),
+        ),
+        bottomNavigationBar: WidgetBottomNavigationBar(),
       ),
-      child: Center(child: Text(text)),
     );
   }
 }
--- lib/widgets/widget_bottombar.dart
+++ lib/widgets/widget_bottombar.dart
@@ -1,9 +1,79 @@
 import 'package:flutter/material.dart';
 import '../screens/main_page.dart';
 import '../screens/my_page.dart';
+import 'widget_drawer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class WidgetBottomNavigationBar extends StatelessWidget {
+class WidgetBottomNavigationBar extends StatefulWidget {
+  @override
+  State<WidgetBottomNavigationBar> createState() =>
+      _WidgetBottomNavigationBarState();
+}
+
+class _WidgetBottomNavigationBarState extends State<WidgetBottomNavigationBar> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  int _selectedIndex = 0;
+
+  bool _isDrawerOpen = false;
+  // Drawer의 상태를 저장할 변수
+  SharedPreferences? _prefs;
+
+  // SharedPreferences 초기화 및 Drawer 상태 복원
+  @override
+  void initState() {
+    super.initState();
+    _initSharedPreferences();
+  }
+
+  // SharedPreferences 초기화
+  _initSharedPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+    _isDrawerOpen = _prefs!.getBool('isDrawerOpen') ?? false;
+    if (_isDrawerOpen) {
+      _openDrawer();
+    }
+  }
+
+  // Drawer 열기
+  void _openDrawer() {
+    setState(() {
+      _isDrawerOpen = true;
+    });
+    _prefs?.setBool('isDrawerOpen', true); // Drawer 상태를 저장
+    Scaffold.of(context).openDrawer();
+  }
+
+  // Drawer 닫기
+  void _closeDrawer() {
+    setState(() {
+      _isDrawerOpen = false;
+    });
+    _prefs?.setBool('isDrawerOpen', false); // Drawer 상태를 저장
+    Navigator.of(context).pop();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if (index == 2) {
+        // 열려 있는 경우 Drawer를 닫습니다.
+        if (_isDrawerOpen) {
+          _closeDrawer();
+        } else {
+          _openDrawer();
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +92,7 @@ class WidgetBottomNavigationBar extends StatelessWidget {
       currentIndex: 1,
       onTap: (int index) {
         if (index == 0) {
-          final scaffoldState = scaffoldKey.currentState;
-          if (scaffoldState != null) {
-            scaffoldState.openDrawer();
-          }
+          _onItemTapped;
         } else if (index == 1) {
           Navigator.push(
             context,
