cdf77bc5a4c0279a71d65a611643d95f6d72309a
hyundo0328 <hyundo13@naver.com>
2023-11-06 16:52:02 +0900

    디자인 정리

--- lib/screens/my_page.dart
+++ lib/screens/my_page.dart
@@ -1,213 +1,161 @@
 // mypage.dart
 import 'package:flutter/material.dart';
-import 'communicate.dart';
-import '../main.dart';
-import 'program_rent.dart';
-import 'main_page.dart';
+import '../widgets/widget_appbar.dart';
+import '../widgets/widget_bottombar.dart';
+import 'email_change.dart';
+import 'phone_change.dart';
 
 class MyPageScreen extends StatelessWidget {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('마이페이지'),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 44, 96, 68),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(55.0), // AppBar의 원하는 높이로 설정
+          child: WidgetAppBar(title: "동구밭"),
         ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: Column(
+        body: SafeArea(
+          top: true,
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(15, 15, 15, 15),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  // mainAxisSize: MainAxisSize.max,
                   children: [
-                    CircleAvatar(
-                      radius: 50.0,
-                      backgroundImage:
-                          AssetImage('assets/user_profile.jpg'),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: Image.asset(
+                        'assets/user_profile.jpg',
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    SizedBox(height: 10.0),
-                    // 사용자 이름
-                    Text(
-                      'hyundo',
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                    SizedBox(height: 5.0),
-                    // 로그인한 이메일
-                    Text(
-                      'hyundo13@naver.com',
-                      style: TextStyle(fontSize: 16.0, color: Colors.grey),
-                    ),
-                    SizedBox(height: 10.0),
-                    // 로그아웃 버튼
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => UserProfilePage()),
-                        );
-                      },
-                      child: Text('내 정보 변경'),
-                      style: ElevatedButton.styleFrom(
-                        primary: const Color.fromARGB(255, 44, 96, 68),
+                    Align(
+                      alignment: AlignmentDirectional(-1.00, 0.00),
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Align(
+                              alignment: AlignmentDirectional(0.00, -1.00),
+                              child: Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(5, 5, 5, 0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          0, 0, 15, 0),
+                                      child: Text(
+                                        '김성동 님',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontFamily: 'Readex Pro'),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          5, 5, 5, 5),
+                                      child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            fixedSize: Size(75, 20),
+                                            padding:
+                                                EdgeInsetsDirectional.fromSTEB(
+                                                    0, 0, 0, 0),
+                                            backgroundColor: Colors.white,
+                                            side: BorderSide(
+                                                color: Color(0xFFC2C2C2),
+                                                width: 1),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      UserProfilePage()),
+                                            );
+                                          },
+                                          child: Text(
+                                            "내 정보 수정",
+                                            style: TextStyle(
+                                              fontFamily: 'Readex Pro',
+                                              color: Color(0xFFC2C2C2),
+                                              fontSize: 11,
+                                            ),
+                                          )),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          5, 5, 5, 5),
+                                      child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                            fixedSize: Size(75, 20),
+                                            padding:
+                                                EdgeInsetsDirectional.fromSTEB(
+                                                    0, 0, 0, 0),
+                                            backgroundColor: Colors.white,
+                                            side: BorderSide(
+                                                color: Color(0xFFC2C2C2),
+                                                width: 1),
+                                          ),
+                                          onPressed: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      UserProfilePage()),
+                                            );
+                                          },
+                                          child: Text(
+                                            "로그아웃",
+                                            style: TextStyle(
+                                              fontFamily: 'Readex Pro',
+                                              color: Color(0xFFC2C2C2),
+                                              fontSize: 11,
+                                            ),
+                                          )),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: AlignmentDirectional(-1.00, 0.00),
+                              child: Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(5, 0, 5, 15),
+                                child: Text('이메일 : donggubat@dgu.kr',
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(
+                                      fontFamily: 'Readex Pro',
+                                      color: Color(0xFFC2C2C2),
+                                    )),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-        drawer: Drawer(
-            child: ListView(
-          padding: EdgeInsets.all(10),
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/vertical_symnbol.jpg'),
-              ),
-              accountName: Text('hyundo'),
-              accountEmail: Text('hyundo13@naver.com'),
-              onDetailsPressed: () {
-                print('arrow is clicked');
-              },
-            ),
-            ListTile(
-              title: Text('소통'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => Communicate()));
-              },
-            ),
-            ListTile(
-              title: Text('프로그램/대관'),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => program_apply()));
-              },
-            ),
-          ],
-        )),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          iconSize: 35,
-          selectedItemColor: const Color.fromARGB(255, 44, 96, 68),
-          unselectedItemColor: Colors.grey,
-          selectedLabelStyle: TextStyle(fontSize: 10),
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: '목록'),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: '마이페이지'),
-          ],
-          currentIndex: 2,
-          onTap: (int index) {
-            if (index == 0) {
-              if (scaffoldKey.currentState!.isDrawerOpen) {
-                scaffoldKey.currentState!.openDrawer();
-              } else {
-                scaffoldKey.currentState!.closeDrawer();
-              }
-            } else if (index == 1) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (BuildContext context) => MainPage(),
-                ),
-              );
-            } else if (index == 2) {
-              // '마이페이지' 아이콘을 클릭한 경우 마이페이지 화면으로 이동
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MyPageScreen()),
-              );
-            }
-          },
-        ));
-  }
-}
-
-class PhoneNumberChangePage extends StatelessWidget {
-  final TextEditingController _phoneNumberController = TextEditingController();
-
-  void _handleChangePhoneNumber() {
-    // 여기에 전화번호 변경 로직을 구현하세요.
-    String newPhoneNumber = _phoneNumberController.text;
-    // 변경된 전화번호를 서버에 업데이트하거나 로컬 저장소에 저장하는 등의 작업을 수행하세요.
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('전화번호 변경'),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 44, 96, 68),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _phoneNumberController,
-              decoration: InputDecoration(labelText: '새로운 전화번호'),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _handleChangePhoneNumber,
-              child: Text('전화번호 변경'),
-              style: ElevatedButton.styleFrom(
-                primary: const Color.fromARGB(255, 44, 96, 68),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class EmailChangePage extends StatelessWidget {
-  final TextEditingController _emailController = TextEditingController();
-
-  void _handleChangeEmail() {
-    // 여기에 이메일 주소 변경 로직을 구현하세요.
-    String newEmail = _emailController.text;
-    // 변경된 이메일 주소를 서버에 업데이트하거나 로컬 저장소에 저장하는 등의 작업을 수행하세요.
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('이메일 주소 변경'),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 44, 96, 68),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: '새로운 이메일 주소'),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _handleChangeEmail,
-              child: Text('이메일 주소 변경'),
-              style: ElevatedButton.styleFrom(
-                primary: const Color.fromARGB(255, 44, 96, 68),
-              ),
-            ),
-          ],
-        ),
+        bottomNavigationBar: WidgetBottomNavigationBar(),
       ),
     );
   }
@@ -222,78 +170,64 @@ class UserProfilePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 44, 96, 68),
       ),
-      body: ListView(
-        children: [
-          // 상단 정보 컨테이너
-          Container(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                // 사용자 아이콘 (여기에 사용자의 프로필 이미지 아이콘을 추가)
-                CircleAvatar(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            // 사용자 아이콘 (여기에 사용자의 프로필 이미지 아이콘을 추가)
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Center(
+                child: CircleAvatar(
                   radius: 50.0,
                   backgroundImage:
                       AssetImage('assets/user_profile.jpg'), // 프로필 이미지 경로
                 ),
-                SizedBox(height: 10.0),
-                // 사용자 이름
-                Text(
-                  'hyundo',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                SizedBox(height: 5.0),
-                // 로그인한 이메일
-                Text(
-                  'hyundo13@naver.com',
-                  style: TextStyle(fontSize: 16.0, color: Colors.grey),
-                ),
-                SizedBox(height: 10.0),
-                // 로그아웃 버튼
-                ElevatedButton(
-                  onPressed: () {
-                    // 로그아웃 로직 추가
-                  },
-                  child: Text('로그아웃'),
-                  style: ElevatedButton.styleFrom(
-                    primary: const Color.fromARGB(255, 44, 96, 68),
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-
-          // 전화번호 변경 버튼
-          ListTile(
-            leading: Icon(Icons.phone),
-            title: Text('전화번호 변경'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              // 전화번호 변경 화면으로 이동
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return PhoneNumberChangePage(); // 전화번호 변경 화면의 클래스
-              }));
-            },
-          ),
-
-          // 이메일 주소 변경 버튼
-          ListTile(
-            leading: Icon(Icons.email),
-            title: Text('이메일 주소 변경'),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              // 이메일 주소 변경 화면으로 이동
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return EmailChangePage(); // 이메일 주소 변경 화면의 클래스
-              }));
-            },
-          ),
-        ],
+            SizedBox(height: 10.0),
+            // 사용자 이름
+            Text(
+              'hyundo',
+              style: TextStyle(fontSize: 25.0),
+            ),
+            SizedBox(height: 5.0),
+            // 로그인한 이메일
+            Text(
+              'hyundo13@naver.com',
+              style: TextStyle(fontSize: 16.0, color: Colors.grey),
+            ),
+            SizedBox(height: 30.0),
+            // 전화번호 변경 버튼
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('전화번호 변경'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // 전화번호 변경 화면으로 이동
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return PhoneNumberChangePage(); // 전화번호 변경 화면의 클래스
+                }));
+              },
+            ),
+            // 이메일 주소 변경 버튼
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('이메일 주소 변경'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // 이메일 주소 변경 화면으로 이동
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return EmailChangePage(); // 이메일 주소 변경 화면의 클래스
+                }));
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
 
 // class ProfileImageChangePage extends StatefulWidget {
 //   @override
