c10d11d7263378d29ca94482938654bc768bfb13
hyundo0328 <hyundo13@naver.com>
2023-11-13 00:13:31 +0900

    프로그램 신청 검색 버튼 추가

--- lib/widgets/widget_appbar.dart
+++ lib/widgets/widget_appbar.dart
@@ -7,10 +7,29 @@ class WidgetAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> actions = [];
+
+    if (title == '프로그램 신청') {
+      // If the title is "프로그램 신청", add the search icon to actions
+      actions.add(
+        Padding(
+          padding: EdgeInsets.only(right: 10.0), // Adjust the padding as needed
+          child: IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Handle search button press
+              // You can navigate to a search page or perform any other action here
+            },
+          ),
+        ),
+      );
+    }
+
     return AppBar(
       centerTitle: true,
       backgroundColor: const Color.fromARGB(255, 44, 96, 68),
-      title: Text(title), // Use the title parameter
+      title: Text(title),
+      actions: actions, // Use the title parameter
     );
   }
 }
