f836fbca4b83114cfba8b65f92b39fca5b469acf
Pinethanku <pinethanku21@gmail.com>
2023-11-13 17:26:18 +0900

    firebase adding...

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 //main.dart
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -162,3 +168,4 @@ class Main extends StatelessWidget {
     );
   }
 }
+
