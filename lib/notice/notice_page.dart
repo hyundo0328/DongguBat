1ded1c96fc1fb5c429892b936535ad250b470b5a
hyundo0328 <hyundo13@naver.com>
2023-11-21 16:15:11 +0900

    공지사항 페이지 db 연결 수정

--- lib/notice/notice_page.dart
+++ lib/notice/notice_page.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
 import '../widgets/widget_appbar.dart';
-import '../widgets/widget_bottombar.dart';
 import 'notice1.dart';
 
 class Notice extends StatelessWidget {
-  int _selectedIndex = 1;
+  final List<String> noticeTitles = [
+    "공지사항 1 제목",
+    "공지사항 2 제목",
+    "공지사항 3 제목",
+    "공지사항 4 제목",
+    "공지사항 5 제목",
+    "공지사항 6 제목",
+    "공지사항 7 제목",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(55.0), // AppBar의 원하는 높이로 설정
+        preferredSize: Size.fromHeight(55.0),
         child: WidgetAppBar(title: "공지사항"),
       ),
       body: Padding(
-        padding: EdgeInsetsDirectional.fromSTEB(15, 15, 15, 15),
+        padding: EdgeInsets.all(15),
         child: ListView(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, top: 20),
+          children: List.generate(noticeTitles.length, (index) {
+            return Padding(
+              padding: EdgeInsets.only(
+                  left: 10, right: 10, bottom: 20, top: index == 0 ? 20 : 0),
               child: Container(
                 alignment: Alignment.centerLeft,
                 width: MediaQuery.of(context).size.width,
@@ -47,7 +55,7 @@ class Notice extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          "공지사항 1 제목",
+                          noticeTitles[index],
                           style: TextStyle(color: Colors.black, fontSize: 20),
                         ),
                         SizedBox(
@@ -62,285 +70,8 @@ class Notice extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, top: 20),
-              child: Container(
-                alignment: Alignment.centerLeft,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black38, width: 1.0),
-                  borderRadius: BorderRadius.circular(6),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 5,
-                      color: Color(0x33000000),
-                      offset: Offset(5, 5),
-                    )
-                  ],
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NoticePage1()),
-                    );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(13),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "공지사항 2 제목",
-                          style: TextStyle(color: Colors.black, fontSize: 20),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "공지 요약 내용",
-                          style: TextStyle(color: Colors.black54, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, top: 20),
-              child: Container(
-                alignment: Alignment.centerLeft,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black38, width: 1.0),
-                  borderRadius: BorderRadius.circular(6),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 5,
-                      color: Color(0x33000000),
-                      offset: Offset(5, 5),
-                    )
-                  ],
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NoticePage1()),
-                    );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(13),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "공지사항 3 제목",
-                          style: TextStyle(color: Colors.black, fontSize: 20),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "공지 요약 내용",
-                          style: TextStyle(color: Colors.black54, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, top: 20),
-              child: Container(
-                alignment: Alignment.centerLeft,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black38, width: 1.0),
-                  borderRadius: BorderRadius.circular(6),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 5,
-                      color: Color(0x33000000),
-                      offset: Offset(5, 5),
-                    )
-                  ],
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NoticePage1()),
-                    );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(13),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "공지사항 4 제목",
-                          style: TextStyle(color: Colors.black, fontSize: 20),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "공지 요약 내용",
-                          style: TextStyle(color: Colors.black54, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, top: 20),
-              child: Container(
-                alignment: Alignment.centerLeft,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black38, width: 1.0),
-                  borderRadius: BorderRadius.circular(6),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 5,
-                      color: Color(0x33000000),
-                      offset: Offset(5, 5),
-                    )
-                  ],
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NoticePage1()),
-                    );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(13),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "공지사항 5 제목",
-                          style: TextStyle(color: Colors.black, fontSize: 20),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "공지 요약 내용",
-                          style: TextStyle(color: Colors.black54, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, top: 20),
-              child: Container(
-                alignment: Alignment.centerLeft,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black38, width: 1.0),
-                  borderRadius: BorderRadius.circular(6),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 5,
-                      color: Color(0x33000000),
-                      offset: Offset(5, 5),
-                    )
-                  ],
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NoticePage1()),
-                    );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(13),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "공지사항 6 제목",
-                          style: TextStyle(color: Colors.black, fontSize: 20),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "공지 요약 내용",
-                          style: TextStyle(color: Colors.black54, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  EdgeInsets.only(left: 15, right: 15, top: 20, bottom: 20),
-              child: Container(
-                alignment: Alignment.centerLeft,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.black38, width: 1.0),
-                  borderRadius: BorderRadius.circular(6),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 5,
-                      color: Color(0x33000000),
-                      offset: Offset(5, 5),
-                    )
-                  ],
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NoticePage1()),
-                    );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(13),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "공지사항 7 제목",
-                          style: TextStyle(color: Colors.black, fontSize: 20),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "공지 요약 내용",
-                          style: TextStyle(color: Colors.black54, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
+            );
+          }),
         ),
       ),
     );
